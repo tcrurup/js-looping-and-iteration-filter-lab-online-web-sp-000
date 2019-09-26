0a6e42f5c64b1e4d2bded358f632e3cbff20ec15
Anthony Rurup <tcrurup@gmail.com>
2019-09-26 18:20:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file
+function findMatching(array, name){
+  
+  const matchingNames = [];
+  
+  for (const userName of array){
+    if (userName.toLowerCase() === name.toLowerCase()){
+      matchingNames.push(userName)
+    }
+  }
+  
+  return matchingNames;
+}
+
+function fuzzyMatch(array, parameters){
+  
+  const matchingNames = [];
+  
+  for (const userName of array){
+    if (userName.slice(0, 2).toLowerCase() === parameters.toLowerCase()){
+      matchingNames.push(userName)
+    }
+  }
+  
+  return matchingNames;
+}
+
+function matchName(driverArray, name){
+  
+  const matchingDrivers = [];
+  
+  for (const driver of driverArray){
+    if(driver.name.toLowerCase() === name.toLowerCase()){
+      matchingDrivers.push(driver)
+    }
+  }
+  
+  return matchingDrivers;
+}
