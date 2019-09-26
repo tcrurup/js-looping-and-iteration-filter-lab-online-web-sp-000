eab82300fef2439fd2a1e35be08f55c6c3735380
Anthony Rurup <tcrurup@gmail.com>
2019-09-26 18:05:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(array, name){
+  
+  const matchingNames = [];
+  
+  for (const userName in array){
+    if userName === name{
+      matchingNames.push(name)
+    }
+  }
+  
+  return matchingNames;
+}
